9c30c3e0cea068ae08bb9508511014f083110f3e
Cabpacob <Cabpacob@github.com>
2021-05-15 18:41:54 +0300

    Fixed aid

--- nfc-lib/src/main/java/com/example/nfc_lib/HostCardEmulatorService.kt
+++ nfc-lib/src/main/java/com/example/nfc_lib/HostCardEmulatorService.kt
@@ -13,12 +13,11 @@ class HostCardEmulatorService : HostApduService() {
         const val STATUS_FAILED = "6F00"
         const val CLA_NOT_SUPPORTED = "6E00"
         const val INS_NOT_SUPPORTED = "6D00"
-        const val AID = "A0000002471001"
+        const val AID = "F2323232323232"
         const val SELECT_INS = "A4"
         const val DEFAULT_CLA = "00"
         const val MIN_APDU_LENGTH = 12
     }
-
 
     override fun onDeactivated(reason: Int) {
         Log.d(TAG, "Deactivated: $reason")
