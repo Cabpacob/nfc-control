a426066a487de12eb88e8116842cbb1295414a1c
Cabpacob <Cabpacob@github.com>
2021-05-21 20:44:25 +0300

    Added 3 ui modes

--- app/src/main/java/com/example/nfccontrol/MainActivity.kt
+++ app/src/main/java/com/example/nfccontrol/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.nfccontrol
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nfc_lib.HostCardEmulatorService
@@ -15,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         if (message == null) {
             messageTextView.text = "Please find a QR code"
         } else {
-            messageTextView.text = message
+            messageTextView.text = "Hold your phone to smart device"
 
             val intentToActivity = Intent(this, HostCardEmulatorService::class.java)
 
@@ -25,16 +28,33 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var messageTextView: TextView
 
+    private fun startCurrentAnimation() {
+        val animDrawable = findViewById<View>(R.id.root_layout).background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        messageTextView = findViewById(R.id.textMessage)
+        messageTextView = findViewById(R.id.textView)
 
         val message = IntentHandler.extractMessage(intent)
 
+        if (message == null) {
+            val layout = findViewById<View>(R.id.root_layout)
+            layout.setBackgroundResource(R.drawable.gradient_no_data_animation)
+        }
+
+        startCurrentAnimation()
         createIntent(message)
+
+
+
+
     }
 
     override fun onNewIntent(intent: Intent?) {
