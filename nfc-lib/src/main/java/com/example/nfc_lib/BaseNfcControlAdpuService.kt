93834d5e9e81dcb7318d6ddc6c8724a618871547
Mikhail Nikonov <nikonovmi@yandex-team.ru>
2021-05-23 04:00:31 +0300

    Implemented nfc reader that emulates smart device

---
+++ nfc-lib/src/main/java/com/example/nfc_lib/BaseNfcControlAdpuService.kt
@@ -0,0 +1,43 @@
+package com.example.nfc_lib
+
+import android.nfc.cardemulation.HostApduService
+import android.os.Bundle
+
+abstract class BaseNfcControlAdpuService : HostApduService() {
+    private var message: String? = null
+
+    fun setMessage(message: String?) {
+        this.message = message
+    }
+
+    override fun processCommandApdu(
+        commandApdu: ByteArray?,
+        extras: Bundle?
+    ): ByteArray {
+        if (commandApdu == null) {
+            return hexStringToByteArray(NFCControlAPI.STATUS_FAILED)
+        }
+
+        val hexCommandApdu = byteArrayToHexString(commandApdu)
+        if (hexCommandApdu.length < NFCControlAPI.MIN_APDU_LENGTH) {
+            return hexStringToByteArray(NFCControlAPI.STATUS_FAILED)
+        }
+
+        if (hexCommandApdu.substring(0, 2) != NFCControlAPI.DEFAULT_CLA) {
+            return hexStringToByteArray(NFCControlAPI.CLA_NOT_SUPPORTED)
+        }
+
+        if (hexCommandApdu.substring(2, 4) != NFCControlAPI.SELECT_INS) {
+            return hexStringToByteArray(NFCControlAPI.INS_NOT_SUPPORTED)
+        }
+
+        val charset = Charsets.UTF_8
+        val textBytes = message?.toByteArray(charset) ?: ByteArray(0)
+
+        return if (hexCommandApdu.substring(10, 24) == NFCControlAPI.AID) {
+            textBytes + hexStringToByteArray(NFCControlAPI.STATUS_SUCCESS)
+        } else {
+            hexStringToByteArray(NFCControlAPI.STATUS_FAILED)
+        }
+    }
+}
